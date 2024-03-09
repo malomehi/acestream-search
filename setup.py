ee6add6f65f3443dc2014053caf735c0d9a1b50f
malomehi <1456960+malomehi@users.noreply.github.com>
2024-03-09 01:13:59 +0100

    Add renovate config and update setup metadata

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import find_packages, setup
 
 with open('requirements.txt') as f:
@@ -10,12 +12,18 @@ console_scripts = [
 
 setup(
     name='acestream-search',
+    author_email='1456960+malomehi@users.noreply.github.com',
+    description='Simple tool to scrape acestream links from HTML content',
     setup_requires=['setuptools-git-versioning'],
     install_requires=required,
     setuptools_git_versioning={
         'enabled': True,
     },
     packages=find_packages(),
+    project_urls={
+        'Bug Reports': 'https://github.com/malomehi/acestream-search/issues',
+        'Source': 'https://github.com/malomehi/acestream-search',
+    },
     entry_points={
         'console_scripts': console_scripts
     }
