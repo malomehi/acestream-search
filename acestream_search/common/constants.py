4f30966ec6219c5ca96022ec31adbf998dbb8529
malomehi <malomehi@users.noreply.github.com>
2024-06-13 00:38:07 +0000

    Update URLs

--- acestream_search/common/constants.py
+++ acestream_search/common/constants.py
@@ -1,5 +1,5 @@
 EVENTS_URL = 'https://livetv.sx'
-ALTERNATIVE_EVENTS_URL = 'https://livetv790.me'
+ALTERNATIVE_EVENTS_URL = 'https://livetv791.me'
 CHANNELS_URL = 'https://viendoelfutbolporlaface.pages.dev'
 
 CATEGORIES = {
