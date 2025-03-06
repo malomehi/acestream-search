65d1db664330c47bd33610c8ebe13e0c124acf57
malomehi <1456960+malomehi@users.noreply.github.com>
2025-03-06 21:47:07 +0100

    Fix 'update-urls' github actions workflow

--- update-urls.sh
+++ update-urls.sh
@@ -5,8 +5,10 @@ urls=$(grep -o 'https\?://[a-zA-Z0-9./?=_&-]\+' acestream_search/common/constant
 for url in $urls
 do
     status_code=$(curl --silent --head --output /dev/null --write-out '%{http_code}' $url)
+    echo "Status code for '$url': $status_code"
     if [ $status_code -eq 301 ]; then
         redirect_url=$(curl --silent --head --output /dev/null --write-out '%{redirect_url}' $url | head -c-1)
         sed -i "s,$url,$redirect_url,g" acestream_search/common/constants.py
+        echo "Updated '$url' to '$redirect_url'"
     fi
 done
