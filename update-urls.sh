9ffa194e827499a066ef6cbdde59c82263188d62
malomehi <1456960+malomehi@users.noreply.github.com>
2025-03-29 01:11:51 +0100

    Check that redirect url is valid before updating

--- update-urls.sh
+++ update-urls.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-urls=$(grep -o 'https\?://[a-zA-Z0-9./?=_&-]\+' acestream_search/common/constants.py)
+regex='https\?://[a-zA-Z0-9./?=_&-]\+'
+urls=$(grep -o "$regex" acestream_search/common/constants.py)
 
 for url in $urls
 do
@@ -8,7 +9,11 @@ do
     echo "Status code for '$url': $status_code"
     if [ $status_code -eq 301 ]; then
         redirect_url=$(curl --silent --head --output /dev/null --write-out '%{redirect_url}' $url | head -c-1)
-        sed -i "s,$url,$redirect_url,g" acestream_search/common/constants.py
-        echo "Updated '$url' to '$redirect_url'"
+        if test -n "$(echo "$redirect_url" | grep -o "$regex")"; then
+            sed -i "s,$url,$redirect_url,g" acestream_search/common/constants.py
+            echo "Updated '$url' to '$redirect_url'"
+        else
+            echo "Invalid redirect url: $redirect_url"
+        fi
     fi
 done
