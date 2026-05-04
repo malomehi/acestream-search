e98945c7bdc2aabac318b46e3f820fe9cba11f89
malomehi <1456960+malomehi@users.noreply.github.com>
2026-05-04 15:42:01 +0000

    Update URLs

--- acestream_search/common/constants.py
+++ acestream_search/common/constants.py
@@ -1,5 +1,5 @@
 EVENTS_URL = 'https://livetv.sx'
-ALTERNATIVE_EVENTS_URL = 'https://livetv881.me'
+ALTERNATIVE_EVENTS_URL = 'https://livetv882.me'
 CHANNELS_URL = 'https://ipfs.io/ipns/elcano.top'
 
 CATEGORIES = {
